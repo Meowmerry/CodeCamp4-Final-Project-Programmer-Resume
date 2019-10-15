5a8e2488fd532303b7f82de6cb83bf956381f5f5
Meowmerry <meow3525@gmail.com>
2019-10-15 16:21:34 +0700

    Committed for modifiled : JajaScript.js and index.html

--- JS/javaScrip.js
+++ JS/javaScrip.js
@@ -1,8 +1,27 @@
 
     // Function to change webpage background color
-    function changeBodyBg(color){
-        document.body.style.background = color;
-    }
+        const changeColor = document.getElementById('changeColor'),
+        colors      = ['red', 'green', 'blue','pink'];
+        let   colorIndex  = 0;
+
+        changeColor.addEventListener('click', () => {
+        document.body.style.backgroundColor = colors[colorIndex];      
+        colorIndex = (colorIndex + 1) % colors.length
+        });
+
+    // Function to change name
+    function mynameFunction() {
+        var name;
+        var username = prompt("Please enter your name:", "Thasanee Puttamadilok");
+        if (username == null || username == "") {
+          name = "User cancelled the prompt.";
+        } else {
+          name = "Hello " + username + "! How are you today?";
+        }
+        document.getElementById("changename").innerHTML = name;
+      }
+
+
     // Function to add and remove skills
     var listitem = 0;
     function addToList() {
